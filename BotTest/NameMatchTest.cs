e34eac4645ce355df1bd42d34b6133785b2981ca
Viktor Korolyuk <viktor.korolyuk@gmail.com>
2020-09-03 17:17:36 -0400

    Update NameMatchTest to test the logic of the algorithm
    Added an option to enable sending certain test outputs to Discord

--- BotTest/NameMatchTest.cs
+++ BotTest/NameMatchTest.cs
@@ -1,6 +1,4 @@
 using Discord;
-using Discord.Commands;
-using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.IO;
@@ -9,25 +7,34 @@ using TU20Bot;
 using TU20Bot.Commands;
 using TU20Bot.Configuration;
 
-namespace BotTest {
+namespace BotTest
+{
     [TestClass]
-    public class NameMatchTest {
+    public class NameMatchTest
+    {
 
         private static NameMatch _nameMatch;
         private static Config _config;
         private static Client _client;
         private static Handler _handler;
 
+        private const bool OUTPUT_TO_DISCORD = false;
+
         [ClassInitialize]
-        public static async Task ClassInitialize(TestContext testContext) {	
+        public static async Task ClassInitialize(TestContext testContext)
+        {
 
             string token = "";
+            testContext.WriteLine("Iteration 1");
 
             // Start bot with token from "token.txt" in working folder.
-            try {
-                var _token = File.ReadAllText("token.txt").Trim();
+            try
+            {
+                var _token = File.ReadAllText("../../../../token.txt").Trim();
                 token = _token.ToString();
-            } catch (IOException) {
+            }
+            catch (IOException)
+            {
                 // current directory: BotTest/bin/Debug/netcoreapp3.1
                 Console.WriteLine("Could not read from token.txt." +
                     " Did you put token.txt file in the current directory?");
@@ -45,26 +52,97 @@ namespace BotTest {
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
 
-            // Waiting for the bot to log in and appear online
-            bool ready = false;
-            _client.Ready += () => {
-                ready = true;
-                return Task.CompletedTask;
-            };
-            while (!ready);
-
             _nameMatch = new NameMatch();
         }
 
         [TestMethod]
-        public async Task CheckMatchName() {
+        public async Task CheckMatchName()
+        {
+
+            // Waiting for the bot to log in and appear online
+            bool ready = false;
+            _client.Ready += () =>
+            {
+                ready = true;
+                return Task.CompletedTask;
+            };
+            while (!ready) ;
 
             var guild = _client.GetGuild(_config.guildId);
             var users = guild.Users;
 
-            // Sending a message to a specified channel in a specified server using the algorithm function
-            await guild.GetTextChannel(_config.welcomeChannelId)
-                .SendMessageAsync(await _nameMatch.nameMatching(_config.origNames, users, null));
+            //Sending a message to a specified channel in a specified server using the algorithm function
+            var stringResponse = await _nameMatch.nameMatching(_config.origNames, users, null);
+
+            // TODO: Add thourough checks to verify the algorithm is working as intended
+
+            if (OUTPUT_TO_DISCORD)
+                await guild.GetTextChannel(_config.welcomeChannelId).SendMessageAsync(stringResponse);
+        }
+
+        [TestMethod]
+        public void TestMatchNameAlgorithm_FullMatch()
+        {
+            string[,] nameSet = { { "John", "Doe" } };
+            NameMatch.nameMatchAlg("John Doe", nameSet, (index) =>
+            {
+                Assert.Fail();
+            }, (index, firstName, lastName) =>
+            {
+
+            }, (index, firstName, lastName) =>
+            {
+                Assert.Fail();
+            });
+        }
+
+        [TestMethod]
+        public void TestMatchNameAlgorithm_LastNameMatch()
+        {
+            string[,] nameSet = { { "Bill", "Doe" } };
+            NameMatch.nameMatchAlg("John Doe", nameSet, (index) =>
+            {
+                Assert.Fail();
+            }, (index, firstName, lastName) =>
+            {
+                Assert.Fail();
+            }, (index, firstName, lastName) =>
+            {
+            });
+        }
+
+        [TestMethod]
+        public void TestMatchNameAlgorithm_NoMatch()
+        {
+            string[,] nameSet = { { "Bill", "Johnson" } };
+            NameMatch.nameMatchAlg("John Doe", nameSet, (index) =>
+            {
+                Assert.Fail();
+            }, (index, firstName, lastName) =>
+            {
+                Assert.Fail();
+            }, (index, firstName, lastName) =>
+            {
+                Assert.Fail();
+            });
+        }
+
+        [TestMethod]
+        public void TestMatchNameAlgorithm_NoSpaceMatch()
+        {
+            string[,] nameSet = { { "John", "Johnson" } };
+            NameMatch.nameMatchAlg("JohnDoe", nameSet, (index) =>
+            {
+                //Assert.Fail();
+            }, (index, firstName, lastName) =>
+            {
+                Assert.Fail();
+            }, (index, firstName, lastName) =>
+            {
+                Assert.Fail();
+            });
         }
     }
+
+
 }
