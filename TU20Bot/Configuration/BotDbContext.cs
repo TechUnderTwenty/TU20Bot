4362c8787a77ab8bfdc719fbf969e695cd6d2723
Muhammad Muizz Zafar <muizzzafar25351964@gmail.com>
2020-09-16 03:18:04 -0400

    initial commit

---
+++ TU20Bot/Configuration/BotDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TU20Bot.Configuration {
+    class BotDbContext : DbContext {
+
+        public DbSet<UnverifiedUser> unverifiedUsers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseNpgsql("Server=127.0.0.1;Port=5432;Database=discordbot;User Id=passportdev;Password=strawhatluffy;");
+        }
+
+
+    }
+}
---
+++ TU20Bot/Configuration/UnverifiedUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TU20Bot.Configuration {
+    class UnverifiedUser {
+        
+        public int Id { get; set; }
+
+        public ulong UserId { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
