8d38f288e39ac7e3341180c1b64d874f0e0cffff
Viktor Korolyuk <8334048+viktorKorolyuk@users.noreply.github.com>
2021-01-13 13:07:41 -0500

    Update AuthenticationController to handle login and creation for many users

--- TU20Bot/Configuration/Controllers/AuthenticationController.cs
+++ TU20Bot/Configuration/Controllers/AuthenticationController.cs
@@ -1,36 +1,59 @@
 ﻿using System;
-using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using EmbedIO;
 using EmbedIO.Routing;
 using EmbedIO.WebApi;
 using Jose;
+using MongoDB.Driver;
+using TU20Bot.Models;
 
 namespace TU20Bot.Configuration.Controllers {
-    public class AuthenticationController: ServerController {
-        public const int HASH_SIZE = 24;
-        private const int ITERATIONS = 1000000;
-        private static readonly string tempSalt = Environment.GetEnvironmentVariable("TEMPSALT") ?? "TEMPSALT";
+    public class AuthenticationController : ServerController {
 
         [Route(HttpVerbs.Post, "/login")]
-        public string Login([QueryField] string password) {
+        public async Task<string> Login([QueryField] string username, [QueryField] string password) {
 
-            var stringHash = hashPassword(password);
-            if(stringHash.Equals(this.server.config.consolePassword))
-                return JWT.Encode(new AuthorizationPayload{
-                    fullName = "Admin",
-                    validUntil = DateTime.Now.AddHours(1) // Expire in 1 hour
+            var user = await server.client.database.GetCollection<AccountModel>(AccountModel.collectionName).FindAsync(_user => _user.username.Equals(username.ToLower()));
+
+            if (!user.Any()) {
+                throw HttpException.Unauthorized();
+            }
+
+            if (user.First().checkPassword(password)) {
+                return JWT.Encode(new AuthorizationPayload {
+                    fullName = username,
+                    validUntil = DateTime.Now.AddHours(1), // Expire in 1 hour
+                    permissions = user.First().permissions
                 }, Encoding.UTF8.GetBytes(this.server.config.jwtSecret), JwsAlgorithm.HS256);
+            }
 
-            return null;
+            throw HttpException.Unauthorized();
         }
 
-        /// <summary>
-        /// Given a password string, create a PBKDF2 Hash
-        /// </summary>
-        public static string hashPassword(string password) {
-            Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(password, Encoding.UTF8.GetBytes(tempSalt != null ? tempSalt : tempSalt_default), ITERATIONS);
-            return Convert.ToBase64String(pbkdf2.GetBytes(HASH_SIZE));
+        [Route(HttpVerbs.Post, "/create")]
+        public async Task<string> Create([QueryField] string username, [QueryField] string password) {
+
+            if (!AuthorizationModule.ValidatePermissions(new List<string> { "Admin" }, Request, Response, server.config.jwtSecret)) {
+                throw HttpException.Forbidden();
+            }
+
+            var user = server.client.database.GetCollection<AccountModel>(AccountModel.collectionName).Find(_user => _user.username.Equals(username.ToLower()));
+
+            // If a user already exists, it is not possible to create a new user
+            if (user.Any()) throw HttpException.NotAcceptable();
+
+            var _user = new AccountModel {
+                username = username.ToLower(),
+                permissions = new List<string> { "Admin" }
+            };
+            _user.setPassword(password);
+
+            await server.client.database.GetCollection<AccountModel>(AccountModel.collectionName).InsertOneAsync(_user);
+
+            return username.ToLower();
         }
     }
 }
